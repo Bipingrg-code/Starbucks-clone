8f5826f11ce9bca26d5b447c116235a52798762a
Bpeen Gurung <bipin67678grg@gmail.com>
2022-04-12 22:00:44 +0545

    add css media queries

---
+++ javascript/main.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('#hamburger');
+const leftMenu = document.querySelector('#l-menu');
+const rightMenu = document.querySelector('#r-menu');
+var menu;
+hamburger.addEventListener('click',() => {
+
+})
